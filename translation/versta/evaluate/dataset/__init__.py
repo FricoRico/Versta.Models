27f3f59c4aea72c0208dc3697bfd88cea5a3a3e3
Ricardo Snoek-Valkenburg <me@ricardoismy.name>
2026-07-15 22:32:37 +0200

    Removing all files related to training, as we will not be focussing on fine-tuning LLMs for the moment

--- translation/versta/evaluate/dataset/__init__.py
+++
@@ -1,8 +0,0 @@
-from .custom import filter_dataset, load_custom_dataset
-from .flores import is_flores_dataset, load_flores_dataset
-
-
-def load_dataset(dataset_path: str, source: str, target: str) -> list[dict]:
-    if is_flores_dataset(dataset_path):
-        return load_flores_dataset(source, target)
-    return load_custom_dataset(dataset_path)
